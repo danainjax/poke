259a80313beba62322515f8246106a2f2c6de019
danainjax <danainjax@gmail.com>
2021-09-27 13:43:16 -0400

    b

--- Poke.js
+++ Poke.js
@@ -10,7 +10,7 @@ class Poke {
     //    const {name, url} = this
         this.all.forEach((poke) => {
             const pokeP = document.createElement('p')
-            document.body.append(pokeP)
+            main.append(pokeP)
             pokeP.innerHTML = `
             <h2>${poke.data.name}</h2>
             `
@@ -19,18 +19,42 @@ class Poke {
     })
 }
 
+    
+
+
  static getPoke () {
     api.getTenPoke().then(pokeJSON => {
         const pokes = pokeJSON.results
         console.log (pokes)
         pokes.forEach((poke) => new Poke(poke));
             this.renderIndex()
+            this.renderLetter()
            
     })
     }
-    
-}
 
+    static renderLetter = () => {
+        
+        
+        document.getElementById('b').addEventListener('click', (e) => {
+            e.preventDefault()
+            Poke.all.filter(poke => {
+                if (poke.data.name.startsWith('b')) {
+                    main.innerHTML = ""
+                    const div = document.createElement('div')
+                    console.log(poke.data.name)
+                    main.append(div)
+                    div.innerHTML += `<p>${poke.data.name}</p>`
+                    
+                    
+    
+            }
+    
+            })
+        })
+    }
+
+}
     
  
 
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const api = new ApiService('https://pokeapi.co/api/v2')
+const main = document.getElementById('main')
 
 // api.getTenPoke()
 Poke.getPoke()
+
+
+document.getElementById('a').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('a button')
+    
+})
+
+
+document.getElementById('c').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('c button')
+})
